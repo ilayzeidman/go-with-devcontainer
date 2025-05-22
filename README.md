03124b55099b7cec0703f792c85d5298865ee531
Ilay Zeidman <izeidman@synamedia.com>
2025-05-22 13:54:12 +0300

    update README and main.go with server setup instructions and API endpoint

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # go-with-devcontainer
+
+run the server:
+go run main.go
+
+invoke the api through unix socket with curl:
+curl --unix-socket ./ctl.sock http://localhost/ctl/terminate
--- src/main.go
+++ src/main.go
@@ -1,5 +1,33 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+)
+
+const CTL_SOCKET_PATH = "./ctl.sock"
+
 func main() {
     fmt.Println("hello world")
+
+    os.Remove(CTL_SOCKET_PATH)
+    http.HandleFunc("/ctl/terminate", func(w http.ResponseWriter, r *http.Request) {
+
+    fmt.Fprintf(w, "Done\n")
+
+	})
+
+    server := http.Server{}
+
+    unixListener, err := net.Listen("unix", CTL_SOCKET_PATH)
+    if err != nil {
+        panic(err)
+    }
+    go server.Serve(unixListener)
+    fmt.Println("Server started and listening on", CTL_SOCKET_PATH)
+    // Block forever
+	select {}
+    
 }
